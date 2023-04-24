59ad57ffc42368a9f61e1793cd45825853b89aad
Luis Antonio <antoniomolina.yampa@gmail.com>
2023-04-24 02:04:33 -0400

    adding functions getProcedencia on Person class

--- Persona.cpp
+++ Persona.cpp
@@ -28,4 +28,46 @@ class Persona
         {
             return nombres + " " + apellidos;
         }
+
+        int getCi()
+        {
+            return ci;
+        }
+        Fecha getFechaNacimiento()
+        {
+            return fechaNacimiento;
+        }
+
+        void setCi(int carnet)
+        {
+            ci = carnet;
+        }
+
+        void setFechaNacimiento(Fecha fecha)
+        {
+            fechaNacimiento = fecha;
+        }
+
+        string getProcedencia()
+        {
+            //calculamos la procedencia en funcion al ultimo digito del ci
+            //cosnideramos el siguiente orden
+            /*
+            1.-La Paz
+            2.-Oruro
+            3.-Potosi
+            4.-Cochabamba
+            5.-Chuquisaca
+            6.-Tarija
+            7.-Pando
+            8.-Beni
+            9.-Santa Cruz
+            */
+
+           //calcular el ultimo digito
+           int ud = ci%10;
+           //creamos un data set acorde a los datos
+           string departamentos[9] {"La Paz","Oruro","Potosi","Cochabamba","Chuquisaca","Tarija","Pando","Beni","Santa Cruz"};
+           return departamentos[ud-1];
+        }
 };
